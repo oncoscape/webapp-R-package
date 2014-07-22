61f8b40ee7e3bd65bc3a38b3573fb0613f9b4f4e
h.bolouri <h.bolouri@1fc4b0cc-ecbe-4240-a69a-c8b6b0a06bd2>
2014-07-22 03:20:53 +0000

    added tests for new DataProviderBridge patientPairedDistribution functions, one of which will soon be deleted.

--- hbolouri/oncoDev/Oncoscape/inst/unitTests/websocketTests/websocket_test_PatientHistoryProviders.R
+++ hbolouri/oncoDev/Oncoscape/inst/unitTests/websocketTests/websocket_test_PatientHistoryProviders.R
@@ -30,6 +30,8 @@ runTests = function (levels)
     test_filterPatientHistory()
     test_getPatientClassification()
     explore_timeLinesData()
+
+    test_calculatePairedDistributionsOfPatientHistoryData()
     
 } # runTests
 #----------------------------------------------------------------------------------------------------
@@ -53,7 +55,7 @@ test_ping <- function()
 test_localFilePreparedTable <- function()
 {
    print("--- test_localFilePreparedTable")
-   cmd <- "getPatientHistory"
+   cmd <- "getTabularPatientHistory"
    status <- "request"
    callback <- "handlePatientHistory"
    payload <- ""
@@ -147,42 +149,106 @@ explore_timeLinesData <- function()
    checkEquals(msg.incoming$cmd, callback)
    checkEquals(msg.incoming$status, "success")
 
-   all.events <- fromJSON(msg.incoming$payload)
-   checkTrue(length(all.events) == 73)
+   all.events <- (msg.incoming$payload)
+   checkTrue(length(all.events) == 1513)
    x <- all.events[[1]]
-   checkEquals(x$PatientID, "FC5PKZ244GQOB098PB2IH2C7X33XO1OT765X")
+   checkEquals(x$PatientID, "TCGA.02.0001")
    checkEquals(x$PtNum, 1)
-   checkEquals(x$Name, "Death")
-   checkEquals(x$date, "06/05/2000")
-   checkEquals(x$StatusQuality, "STD")
+   checkEquals(x$Name, "DOB")
+   checkEquals(x$date, "09/15/1957")
 
-
-   cmd <- "getCaisisPatientHistory";
-   status <- "request"
-   callback <- "handleCaisisPatientHistory"
-   sample.file.2 <- "BTC_clinicaldata_6-18-14.RData" # no subdirectory
-   
-   websocket_write(toJSON(list(cmd=cmd, callback=callback, status=status, payload=sample.file.2)), client)
-
-   system("sleep 1")
-   service(client)
-   checkEquals(names(msg.incoming), c("cmd", "callback", "status", "payload"))
-
-   checkEquals(msg.incoming$cmd, callback)
-   checkEquals(msg.incoming$status, "success")
-
-   all.events <- fromJSON(msg.incoming$payload)
-   checkTrue(length(all.events) == 2581)
-   checkEquals(all.events[[1]],
-               list(PatientID="P1",
-                    PtNum=1,
-                    Name="Chemo",
-                    date=c("7/12/2006", "8/22/2006"),
-                    Type="Temozolomide"))
-
-   all.event.names <- sort(unique(unlist(lapply(all.events, function(event) event$Name))))
-   checkEquals(all.event.names,
-               c("Chemo", "DOB", "Death", "Diagnosis", "Encounter", "MRI", "OR", "Progression", "Radiation"))
    
 } # explore_timeLinesData
+#----------------------------------------------------------------------------------------------------
+test_createRandomPatientPairedDistributionsForTesting <- function()
+{
+   print("--- test_createRandomPatientPairedDistributionsForTesting")
+
+   cmd <- "createRandomPatientPairedDistributionsForTesting"
+   status <- "request"
+   callback <- "pairedDistributionsPlot"
+   payload <- list(popSize1=20, popSize2=30);
+
+   websocket_write(toJSON(list(cmd=cmd, callback=callback, status=status, payload=payload)), client)
+   system("sleep 1")
+   service(client)
+
+   pop1 <- msg.incoming$payload$pop1
+   pop2 <- msg.incoming$payload$pop2
+   checkEquals(length(pop1), 20)
+   checkEquals(length(pop2), 30)
+
+   
+
+} # test_createRandomPatientPairedDistributionsForTesting
+#----------------------------------------------------------------------------------------------------
+test_calculatePairedDistributionsOfPatientHistoryData <- function()
+{
+   print("--- test_calculatePairedDistributionsOfPatientHistoryData")
+
+        #----------------------
+        # first do a test
+        #----------------------
+
+   cmd <- "calculatePairedDistributionsOfPatientHistoryData"
+   status <- "request"
+   callback <- "pairedDistributionsPlot"
+   payload <- list(mode="test", attribute="FirstProgression")
+
+   websocket_write(toJSON(list(cmd=cmd, callback=callback, status=status, payload=payload)), client)
+   system("sleep 1")
+   service(client)
+
+   pop1 <- msg.incoming$payload$pop1
+   pop2 <- msg.incoming$payload$pop2
+   checkTrue(length(pop1) > 10)
+   checkTrue(length(pop2) > 10)
+   checkEquals(length(intersect(names(pop1), names(pop2))), 0)
+
+     # don't know what mean should be, in the general case, but the data should support
+     # the calculation
+
+   pop1.mean <- mean(unlist(pop1, use.names=FALSE))
+   checkTrue(is.numeric(pop1.mean))
+   checkTrue(pop1.mean > 0.0)
+   pop2.mean <- mean(unlist(pop2, use.names=FALSE))
+   checkTrue(is.numeric(pop2.mean))
+   checkTrue(pop2.mean > 0.0)
+
+      #----------------------------------------------------------------------------------
+      # now do a 'real' run.  begin by getting some patientIDs from the currently loaded
+      # patientHistoryTable.  in test mode, this should be
+      # patientHistoryTable: tbl://tcgaGBM/tbl.ptHistory.RData
+      #----------------------------------------------------------------------------------
+   
+   pop1 <- c("TCGA.02.0001", "TCGA.02.0003", "TCGA.02.0006", "TCGA.02.0007", "TCGA.02.0009", "TCGA.02.0010",
+             "TCGA.02.0011", "TCGA.02.0014", "TCGA.02.0021", "TCGA.02.0024")
+   pop2 <- c("TCGA.12.0703", "TCGA.12.0707", "TCGA.12.0769", "TCGA.12.0772", "TCGA.12.0773", "TCGA.12.0775",
+             "TCGA.12.0776", "TCGA.12.0778", "TCGA.12.0780",  "TCGA.15.0742")
+
+   checkEquals(length(intersect(pop1, pop2)), 0)
+
+   cmd <- "calculatePairedDistributionsOfPatientHistoryData"
+   status <- "request"
+   callback <- "pairedDistributionsPlot"
+   payload <- list(attribute="FirstProgression", pop1=pop1, pop2=pop2)
+
+   websocket_write(toJSON(list(cmd=cmd, callback=callback, status=status, payload=payload)), client)
+   system("sleep 1")
+   service(client)
+
+       #----------------------------------------
+       # check the results
+       #----------------------------------------
+
+   pop1.vals <- msg.incoming$payload$pop1
+   pop2.vals <- msg.incoming$payload$pop2
+   checkEquals(names(pop1.vals), pop1)
+   checkEquals(names(pop2.vals), pop2)
+
+   checkEqualsNumeric(mean(as.numeric(pop1.vals)), 1.551, tol=1e-5)
+      # utter mystery: why is pop1 a numeric vector, and pop2 a list?
+   checkEquals(mean(unlist(pop2.vals, use.names=FALSE)), 0.8222222, tol=1e-5)
+
+} # test_calculatePairedDistributionsOfPatientHistoryData
 #----------------------------------------------------------------------------------------------------
