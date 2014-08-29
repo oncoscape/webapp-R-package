ffe98bf6faa9d0aa4192ffeb064ac048911f56a4
l.mcferrin <l.mcferrin@1fc4b0cc-ecbe-4240-a69a-c8b6b0a06bd2>
2014-08-29 17:01:19 +0000

    with global js file

---
+++ hbolouri/oncoDev/Oncoscape/inst/scripts/sendSelectionMenu.js
@@ -0,0 +1,28 @@
+var ActiveModules = [];
+
+//----------------------------------------------------------------------------------------------------
+addSelectionDestination = function(module)
+{
+   if(module in ActiveModules){
+        alert("Selection Destination message handler for '" +  module + " already set");
+   } else{  ActiveModules.push(module)  }
+}
+
+//----------------------------------------------------------------------------------------------------
+getSelectionDestinations = function(){
+     return ActiveModules;
+//   return tissueMenu.children().map(function() {return $(this).val();}).get();
+}
+
+//--------------------------------------------------------------------------------------------
+function SendSelectionToModule(ModuleName, currentIDs){
+    
+      callback = ModuleName + "HandlePatientIDs";    // genralize to "HandleSelectedIDs"?
+      msg = {cmd:"sendPatientIDsToModule",                  // generalize to "sendIDsToModule"?
+             callback: callback,
+             status:"request",
+             payload:{targetModule: ModuleName,
+                      ids: currentIDs}
+             };
+      socket.send(JSON.stringify(msg));
+    }
